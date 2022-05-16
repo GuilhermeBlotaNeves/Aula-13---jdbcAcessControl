c83f58da10c687cda215df5426f8dd7a1cb96f68
Guilherme Blota Neves <101132141+GuilhermeBlotaNeves@users.noreply.github.com>
2022-05-16 16:19:17 -0300

    Update User.java

--- Aula 13 - jdbcAcessControl/src/java/model/User.java
+++ Aula 13 - jdbcAcessControl/src/java/model/User.java
@@ -21,23 +21,26 @@ public class User {
     private long passwordHash;
     private String name;
     
-     public static ArrayList<User> getAllUsers() throws Exception{
-        ArrayList<User> list = new ArrayList<>();
+    public static User getUser(String username, String password) throws Exception{
+        User user = null;
         Class.forName("org.sqlite.JDBC");
         String url = "jdbc:sqlite:mytasks.db";
         Connection con = DriverManager.getConnection(url);
-        Statement stmt = con.createStatement();
-        ResultSet rs = stmt.executeQuery
-        ("select * from users order by name");
-        while(rs.next()){
-            list.add(new User(
+        PreparedStatement stmt = con.prepareStatement(
+            "select * from users where username = ? and pass_hash = ?"
+        );
+        stmt.setString(1, username);
+        stmt.setLong(2, (username+password).hashCode());
+        ResultSet rs = stmt.executeQuery();
+        if(rs.next()){
+            user = new User(
                     rs.getString("username"), 
                     rs.getLong("pass_hash"), 
                     rs.getString("name")
-            ));
+            );
         }
         rs.close(); stmt.close(); con.close();
-        return list;
+        return user;
     }
     
     public User(String username, long passwordHash, String name) {
